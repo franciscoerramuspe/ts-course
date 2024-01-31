a1be0a4fc8371436ac967daa7a30bc961bf8bd3a
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-01-31 10:56:15 -0500

    arrays

---
+++ 4-arrays/arrays.ts
@@ -0,0 +1,19 @@
+const carMakers = ['toyota', 'chevrolet', 'chevy']; //infering type str
+
+const dates = [new Date(), new Date()]; //infering type Date
+
+const carsByMake = [['corolla'], ['tiguan'], ['passat']]; //infering 2d str array
+
+//inference str
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//prevent incompatible values
+carMakers.push(100);
+
+//help with map
+carMakers.map((car: string): string => {
+  return car.toUpperCase(); //knowing that we receive a str and return a str, ts loads for us the str methods
+});
+
+const importantDates = [new Date(), '2030-10-10'];
